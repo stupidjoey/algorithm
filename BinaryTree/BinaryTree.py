bbad9dfce0ecd66971b58a4bc4d51e12fe63b084
stupidjoey <stupidzy1991@gmail.com>
2015-05-15 16:35:41 +0800

    update

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -13,21 +13,15 @@ class BinaryTree():
     def get_root(self):
         return self.root
         
-    def search(self,k):
-        if self.root == None:
-            return None
+    def search(self,root,k):
+        if root == None or root.val == k:
+            return root
         else:
-            current_root = self.root
-            while current_root != None:
-                if current_root.val == k:
-                    return current_root
-                else:
-                    if k < current_root.val:
-                        current_root = current_root.left
-                    elif k > current_root.val:
-                        current_root = current_root.right
-            return None            
-        
+            if k < root.val:
+                return search(root.left,k)
+            else:
+                return search(root.right,k)
+                
     def predecessor(self, node):
         if node.left != None:
             return self.maximum(node.left)
@@ -48,90 +42,81 @@ class BinaryTree():
                 suc = suc.par
             return suc
                           
-    def minimum(self,node):
-        min_node = node
-        current_root = node
-        while current_root.left != None:
-            min_node = current_root.left.val
-            current_root = current_root.left
-        return min_node    
+    def minimum(self,root):
+        while root.left != None:
+            root = root.left
+        return root    
             
     def maximum(self,node):
-        max_node = node
-        current_root = node
-        while current_root.right != None:
-            max_node = current_root.right.val
-            current_root = current_root.right
-        return max_node
+        while root.right != None:
+            root= root.right
+        return root
+
     
-    def delete(self, node):
+    def delete(self,node):
         if node.left == None or node.right == None:
-            parent = node.par
-            if node.right != None:
-                node.right.par = parent
-                if parent.right == node:
-                    parent.right = node.right
-                else:
-                    parent.left = node.right
+            delnode = node
+        else:
+            delnode = self.successor(node)
+        childnode = None
+        delnodepar = delnode.par
+        if delnode.left != None:
+            childnode = delnode.left
+        else:
+            childnode = delnode.right
+        if childnode != None:    # in case of delnode have no child
+            childnode.par = delnode
+        if delnodepar != None:     # in case of delnode is the root
+            if delnode == delnodepar.left:
+                delnodepar.left = childnode
             else:
-                if parent.right == node:
-                    parent.right = node.left
-                else:
-                    parent.left = node.left
-                if node.left != None:
-                    node.left.par = parent                           
-        else:  #case 3  have two children
-            suc = self.successor(node)
-            # the suc of this node won't have left child
-            # delete the suc
-            sucpar = suc.par
-            if sucpar.right == suc:
-                sucpar.right = suc.right
-            else:
-                sucpar.left = suc.right
-            if suc.right != None:
-                suc.right.par = suc.par
-            
-  
+                delnodepar.right = childnode
+        else:
+            self.root = childnode
+        
+        if delnode != node:
+            node.val = delnode.val
+        return delnode
+        
     
-    def insert(self, node):
+    def insert(self,node):
         if self.root == None:
             self.root = node
         else:
-            current_root = self.root 
+            current_root = self.root
+            rootpar = current_root.par
             while current_root != None:
+                rootpar = current_root
                 if node.val < current_root.val:
-                    current_par = current_root
                     current_root = current_root.left
-                    mark = 'left'
                 elif node.val > current_root.val:
-                    current_par = current_root
                     current_root = current_root.right
-                    mark = 'right'
-        
-            if mark == 'left':
-                current_par.left = node
-                node.par = current_par
-            elif mark == 'right':
-                current_par.right = node
-                node.par = current_par
+            node.par = rootpar
+            if node.val < rootpar.val:
+                rootpar.left = node
+            else:
+                rootpar.right = node
+  
                 
-    def inorder_traversal(self,node):
-        if node == None:
+    def inorder_traversal(self,root):
+        if root == None:
             return []
         else:
             traversal = []
-            traversal.extend(self.inorder_traversal(node.left))
-            traversal.append(node.val)
-            traversal.extend(self.inorder_traversal(node.right))          
+            traversal.extend(self.inorder_traversal(root.left))
+            traversal.append(root.val)
+            traversal.extend(self.inorder_traversal(root.right))          
             return traversal
         
                
 binary_tree = BinaryTree()
+
+root_node = binary_tree.get_root()
 node5 = TreeNode(5)
 binary_tree.insert(node5)
 node2 = TreeNode(2)
 binary_tree.insert(node2)
+root_node = binary_tree.get_root()
 
 node3 = TreeNode(1)
 binary_tree.insert(node3)
@@ -139,9 +124,16 @@ node4 = TreeNode(8)
 binary_tree.insert(node4)
 root_node = binary_tree.get_root()
 
+print root_node.val
+print binary_tree.inorder_traversal(root_node)
+print binary_tree.delete(node4).val
 print binary_tree.inorder_traversal(root_node)
 
-node = binary_tree.search(5)
-print binary_tree.predecessor(node).val
-print binary_tree.successor(node).val
+# print binary_tree.inorder_traversal(root_node)
+# node = binary_tree.search(5)
+# binary_tree.delete(node)
+
+
+# print binary_tree.inorder_traversal(root_node)
+
 
